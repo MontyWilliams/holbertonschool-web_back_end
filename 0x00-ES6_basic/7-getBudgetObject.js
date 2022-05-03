f326c58a9c77643268055a25b29ade3cd089582b
Montywilliams <3650@holbertonschool.com>
2022-05-02 19:34:07 -0600

    Modify function’s budget object to simply use only the keyname

---
+++ 0x00-ES6_basic/7-getBudgetObject.js
@@ -0,0 +1,10 @@
+//  didnt have to use this.income this'll be a good one to talk about for pld
+export default function getBudgetObject(income, gdp, capita) {
+  const budget = {
+    income,
+    gdp,
+    capita,
+  };
+
+  return budget;
+}
