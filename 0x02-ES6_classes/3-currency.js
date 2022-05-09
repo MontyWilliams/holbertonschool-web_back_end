46913cbe1a9e506ef59b344f9f78ea78db12f54f
Montywilliams <3650@holbertonschool.com>
2022-05-09 15:49:30 -0600

    implement a class and return the arguments that are given

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,19 @@
+export default class Currency {
+  constructor(code, name){
+    this._code = code;
+    this._name = name;
+  }
+get name() {return this._name; }
+get code(){ return this._code; }
+set code(newCode) {
+  if (typeof newCode !== 'string') { throw Error('Currency must be a string'); }
+  this._code = newCode;
+}
+set name(newName) {
+  if (typeof newName !== 'string') { throw Error('Name must be a string'); }
+  this._name = newName;
+}
+displayFullCurrency() {
+  return `${this.name} (${this.code})`;
+}
+}
