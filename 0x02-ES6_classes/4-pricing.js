79a71344680c40d590d7ed0baac5ce1907ab0818
Montywilliams <3650@holbertonschool.com>
2022-05-09 16:15:47 -0600

    use a static method to convert the price but it isnt being used

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,25 @@
+import Currency from './3-currency'
+export default class Pricing {
+  constructor(amount, currency) {
+  this._amount = amount;
+  this._currency = currency;
+  }
+  get amount() {return this._amount}
+  get currency() {return this._currency}
+  set amount(newAmount) {
+    if (typeof newAmount !== 'string') { throw Error('Currency must be a string'); }
+    this._code = newAmount;
+  }
+
+  set currency(newCurrency) {
+    if (typeof newCurrency !== 'string') { throw Error('Name must be a string'); }
+    this._name = newCurrency;
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
+  }
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}
