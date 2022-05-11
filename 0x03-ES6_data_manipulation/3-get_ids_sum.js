86035ca976a334bfe3a90f1a5057c98b9e8f6315
Montywilliams <3650@holbertonschool.com>
2022-05-11 08:53:59 -0600

    eslint correction

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,7 +1,7 @@
 const getListStudentIds = (students) => {
   if (!Array.isArray(students)) return 0;
-  
-   return students.reduce((previousValue, currentValue) => (previousValue + currentValue.id), 0);
+
+  return students.reduce((previousValue, currentValue) => (previousValue + currentValue.id), 0);
 };
 
 export default getListStudentIds;
