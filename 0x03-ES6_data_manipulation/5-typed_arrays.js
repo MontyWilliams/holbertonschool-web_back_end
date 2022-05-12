0db47005708dd7ae79ee367be080df70c60c3d13
Montywilliams <3650@holbertonschool.com>
2022-05-11 21:02:15 -0600

    eslint correction

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -3,6 +3,5 @@ const createInt8TypedArray = (length, position, value) => {
   const int8view = new Int8Array(buffer);
   int8view[position] = value;
   return new DataView(buffer, int8view);
-  
-}
+};
 export default createInt8TypedArray;
