cfb4130f41e77e140f36b9392335853fa390ffd2
Montywilliams <3650@holbertonschool.com>
2022-08-30 04:14:44 -0600

    add documentation for function

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -4,6 +4,9 @@ annotations
 """
 
 
-def concat(str1:str, str2:str) -> str: 
+def concat(str1:str, str2:str) -> str:
+    """
+    Concatenate a string into a string
+    """
     string = (str1 + str2)
     return (string)
