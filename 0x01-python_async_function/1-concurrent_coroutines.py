39cd049800956d4b59061475648bb01648293742
Montywilliams <3650@holbertonschool.com>
2022-09-02 13:38:54 -0600

    this task was entirleyto difficult, could have used better hints

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,1 +1,20 @@
 #!/usr/bin/env python3
+"""coroutines
+"""
+wait_random = __import__ ('0-basic_async_syntax').wait_random
+import asyncio
+from typing import List
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """spawns wait_random n amount of times"""
+    list = []
+    list2 = []
+    i = 0
+    while (i < n): 
+       num = list.append(wait_random(max_delay))
+       i += 1
+    for cor in asyncio.as_completed(list):
+        list2.append(await cor)
+    return(list2)
+    
