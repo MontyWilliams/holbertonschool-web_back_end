ce87284bf4cc9e7abd6663cd31a796b539fd44e5
Montywilliams <3650@holbertonschool.com>
2022-09-02 22:37:29 -0600

    runtime

---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+""" Import asy
+
+
+
+
+    explain it to yourself. """
+from typing import List
+from time import time
+from asyncio import gather
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    """ Run time for four parallel comprehensions """
+    start = time()
+    tasks = [async_comprehension() for i in range(4)]
+    await gather(*tasks)
+    end = time()
+    return (end - start)
