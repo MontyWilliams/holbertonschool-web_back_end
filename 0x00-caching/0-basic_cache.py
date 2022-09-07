dac0d39b4b3efa02532979a3d0173915b24fda18
Montywilliams <3650@holbertonschool.com>
2022-09-07 02:05:58 -0600

    kinda dissapointed at these vague instructions

---
+++ 0x00-caching/0-basic_cache.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" Create the cache prototype
+"""
+from base_caching import BaseCaching
+
+class BasicCache(BaseCaching):
+    
+
+    def put(self, key, item):
+        """ Add an item in the cache
+        """
+        if key and item:
+            self.cache_data[key] = item
+        else:
+            pass
+
+    def get(self, key):
+        """ Get an item by key
+        """
+        if key and self.cache_data.get(key):
+            return self.cache_data.get(key)
+        return None
