18f491e0788760aac557a4c5d61cabceba9a417a
Montywilliams <3650@holbertonschool.com>
2022-09-12 18:07:08 -0600

    pycode and syntax

--- 0x01-pagination/0-simple_helper_function.py
+++ 0x01-pagination/0-simple_helper_function.py
@@ -2,18 +2,17 @@
 """ simple pagination helper
 """
 
-def index_range(page: int, page_size:int) -> tuple:
+
+def index_range(page: int, page_size: int) -> tuple:
     """Get start and ending position
     """
     if page == 1:
         start = page
         end = page_size
     else:
-        start = ((page -1) * page_size)
+        start = ((page - 1) * page_size)
         print(start)
-        end = (start  + page_size)
+        end = (start + page_size)
     result = (start, end)
 
     return result
-
-    
