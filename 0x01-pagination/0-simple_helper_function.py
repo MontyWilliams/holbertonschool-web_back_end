28e7e1ef7472e2963b95249f47bec2618474b5b1
Montywilliams <3650@holbertonschool.com>
2022-09-12 21:25:03 -0600

    was over thinking the problem

--- 0x01-pagination/0-simple_helper_function.py
+++ 0x01-pagination/0-simple_helper_function.py
@@ -6,14 +6,8 @@
 def index_range(page: int, page_size: int) -> tuple:
     """Get start and ending position
     """
-    if page == 1:
-        start = page
-        end = page_size
-    elif page < 1 or page_size < 1:
-        exit(1)
-    else:
-        start = ((page - 1) * page_size)
-        end = (start + page_size)
+    start = ((page - 1) * page_size)
+    end = (start + page_size)
     result = (start, end)
 
     return result
