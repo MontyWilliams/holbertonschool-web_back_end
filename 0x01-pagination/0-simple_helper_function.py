acf48497f3af29d4696c4bf0798634fd4dbcddfa
Montywilliams <3650@holbertonschool.com>
2022-09-12 18:01:57 -0600

    simple helper

---
+++ 0x01-pagination/0-simple_helper_function.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+""" simple pagination helper
+"""
+
+def index_range(page: int, page_size:int) -> tuple:
+    """Get start and ending position
+    """
+    if page == 1:
+        start = page
+        end = page_size
+    else:
+        start = ((page ) * page_size)
+        print(start)
+        end = (start  + page_size)
+    result = (start, end)
+
+    return result
+
+    
