5619e005ab0a70541ab53928178220ec80f29e6c
Montywilliams <3650@holbertonschool.com>
2022-09-14 21:23:18 -0600

    the regex was to difficult Im just ginna say it

---
+++ 0x02-personal_data/filtered_logger.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+""" Obfuscate and return log message
+"""
+import re
+
+
+def filter_datum(fields: list, redaction: str, message: str, separator: str):
+    """ replace occurrences of certain fields
+        the log message passed in will be replaced
+        with xxx
+    """
+    for field in fields:
+        message = re.sub(field + '=' + '.+?' + separator,
+                         field + '=' + redaction + separator,
+                         message)
+        
+        return(message)
