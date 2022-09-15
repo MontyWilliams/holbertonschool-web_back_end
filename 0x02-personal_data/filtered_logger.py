ddbb708f476a80ca58b805be3fed44ce626ea4e0
Montywilliams <3650@holbertonschool.com>
2022-09-14 22:19:19 -0600

    dreate logger

--- 0x02-personal_data/filtered_logger.py
+++ 0x02-personal_data/filtered_logger.py
@@ -6,6 +6,9 @@ from typing import List
 import logging
 
 
+PII_FIELDS = ('name', 'email', 'phone', 'ssn', 'password')
+
+
 class RedactingFormatter(logging.Formatter):
     """ Redacting Formatter class
         """
@@ -24,6 +27,25 @@ class RedactingFormatter(logging.Formatter):
         return filter_datum(
             self.fields,
             self.REDACTION, super().format(record), self.SEPARATOR)
+
+    
+def get_logger() -> logging.Logger:
+    """
+    Implement a get_logger function that takes no arguments
+    and returns a logging.Logger object.
+    """
+    logger = logging.getLogger('user_data')
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+    # Create console handler (ch) set level info
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.INFO)
+    # streamhandler with redacting formatter
+    ch.setFormatter(RedactingFormatter)
+    # add handler to logger
+    logger.addHandler(ch)
+
+    return logger
 
 
 def filter_datum(fields: List[str],
