3fd2e6d7d87006d816a2e9db1ce7f92cc9ea7a90
Montywilliams <3650@holbertonschool.com>
2022-09-22 12:01:04 -0600

    add doc

--- 0x03-Basic_authentication/api/v1/auth/auth.py
+++ 0x03-Basic_authentication/api/v1/auth/auth.py
@@ -11,18 +11,18 @@ class Auth():
 	def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
 		""" Bad documentation for this task
 		"""
-		# if not path or not excluded_paths:
-		# 	return True
-		# if path[-1] != '/':
-		# 	path += '/'
-		# if path in excluded_paths:
-		# 	return False
 		return False
 
+
 	def authorization_header(self, request=None) -> str:
+		""" ath header
+		"""
 		return None
 
+
 	def current_user(self, request=None) -> TypeVar('User'):
+		""" current user
+		"""
 		return None
 	
 	
