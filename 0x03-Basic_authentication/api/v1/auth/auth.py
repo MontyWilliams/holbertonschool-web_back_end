2c76112a695029e25bd4a6c4a44e5c874c89f0fb
Montywilliams <3650@holbertonschool.com>
2022-09-22 12:07:49 -0600

    pep8

--- 0x03-Basic_authentication/api/v1/auth/auth.py
+++ 0x03-Basic_authentication/api/v1/auth/auth.py
@@ -5,24 +5,21 @@
 from flask import request
 from typing import List, TypeVar
 
+
 class Auth():
-	""" Authentification
-	"""
-	def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-		""" Bad documentation for this task
-		"""
-		return False
+    """ Authentification
+    """
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ Bad documentation for this task
+        """
+        return False
 
+    def authorization_header(self, request=None) -> str:
+        """ ath header
+        """
+        return None
 
-	def authorization_header(self, request=None) -> str:
-		""" ath header
-		"""
-		return None
-
-
-	def current_user(self, request=None) -> TypeVar('User'):
-		""" current user
-		"""
-		return None
-	
-	
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ current user
+        """
+        return None
