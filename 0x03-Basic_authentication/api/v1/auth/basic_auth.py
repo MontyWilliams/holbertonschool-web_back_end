5fd908afd1208739ffb9bad8141dea80fec275b2
Montywilliams <3650@holbertonschool.com>
2022-09-23 13:39:51 -0600

    added logic to check header of user authorization

--- 0x03-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x03-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,9 +2,20 @@
 """
 Basic Authenication
 """
+import base64
 from api.v1.auth.auth import Auth, TypeVar
 
 
 class BasicAuth(Auth):
     """ inherit from auth
     """
+    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+        if authorization_header == None:
+            return None
+        if not isinstance(authorization_header, str):
+            return None
+        if not authorization_header.startswith("Basic "):
+            return None
+        
+        base = authorization_header.split(' ')
+        return base[1]
