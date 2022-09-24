eb4e66cd3e29fee552eba713e503f482a8bdb334
Montywilliams <3650@holbertonschool.com>
2022-09-23 20:10:05 -0600

    init

---
+++ 0x04-Session_authentication/api/v1/auth/auth.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+""" Athentification
+"""
+
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth():
+    """ Authentification
+    """
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ Bad documentation for this task
+        """
+        if not path or not excluded_paths:
+            return True
+        if path[-1] != '/':
+            path += '/'
+        if path in excluded_paths:
+            return False
+        return True
+
+    def authorization_header(self, request=None) -> str:
+        """ ath header. If request doesn’t contain header, none
+            Otherwise, return the value of the header request Authorizatio
+        """
+        if request is None:
+            return None
+        if not request.headers.get("Authorization"):
+            return None
+        return request.headers.get("Authorization")
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ current user
+        """
+        return None
