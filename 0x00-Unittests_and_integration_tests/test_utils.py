8a54dfa3e4f13186a0baac0ec7e69f1e6f60aaf5
Montywilliams <3650@holbertonschool.com>
2022-10-03 19:50:03 -0600

    correction

--- 0x00-Unittests_and_integration_tests/test_utils.py
+++ 0x00-Unittests_and_integration_tests/test_utils.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 """ Tests for utils.py """
+from typing import Sequence
 from parameterized import parameterized
 import unittest
 from utils import access_nested_map
+from typing import Mapping, Sequence, Any
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -13,9 +15,9 @@ class TestAccessNestedMap(unittest.TestCase):
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
         ({"a": {"b": 2}}, ("a", "b"), ({"b": 2}, 2))
     ])
-    def test_access_nested_map(self, nested_map, path, expected_result):
+    def test_access_nested_map(self, nested_map: Mapping, path: Sequence, expect: Any):
         """ Test for correct functioning"""
-        self.assertEqual(access_nested_map(nested_map, path), expected_result)
+        self.assertEqual(access_nested_map(nested_map, path), expect)
 
 if __name__ == '__main__':
     unittest.main()
