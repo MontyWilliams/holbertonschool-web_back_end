a6651c86626ed860c17bf1d5292eee1240d1a6e8
Montywilliams <3650@holbertonschool.com>
2022-10-07 14:54:59 -0600

    import _ from flask_babel

--- 0x01-i18n/3-app.py
+++ 0x01-i18n/3-app.py
@@ -2,7 +2,7 @@
 """ Basic flask app
 """
 from flask import Flask, render_template, request
-from flask_babel import Babel
+from flask_babel import Babel, _
 
 app = Flask(__name__)
 babel = Babel(app)
