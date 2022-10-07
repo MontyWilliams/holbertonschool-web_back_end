6471673d3384ed3e78afb07b4ed705f796110509
Montywilliams <3650@holbertonschool.com>
2022-10-06 22:52:15 -0600

    add

---
+++ 0x01-i18n/3-app.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+""" Basic flask app
+"""
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+app = Flask(__name__)
+babel = Babel(app)
+_.__doc__ = "gettext documentation"
+
+
+class Config(object):
+    """ Configure class is passed in object which is a
+        default value in pythonthat isnherits from the base class
+        but possess no attributes and cant be given any
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+_('home_title')
+_('home_header')
+
+
+@babel.localeselector
+def get_locale():
+    """ get locale
+    """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/', methods=['GET'])
+def index():
+    """ index page
+    """
+    return render_template('1-index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
