94d7e957b489e2f785c8902e10d0e9efe1be19f9
Montywilliams <3650@holbertonschool.com>
2022-10-06 20:30:43 -0600

    import and initialixe babel: add config class for babel and set parameter: wet config for app to initialize from created config

---
+++ 0x01-i18n/1-app.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+""" Basic flask app
+"""
+from flask import Flask, render_template
+from flask_babel import Babel
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config(object):
+    """ Configure class is passed in object which is a 
+        default value in pythonthat isnherits from the base class 
+        but possess no attributes and cant be given any
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+
+
+@app.route('/', methods=['GET'])
+def index():
+    """ index page
+    """
+    return render_template('1-index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
