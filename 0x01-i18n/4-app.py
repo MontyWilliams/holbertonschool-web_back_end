82851110bb9ffe5b7fc92e281a964f770ea37e60
Montywilliams <3650@holbertonschool.com>
2022-10-07 16:05:32 -0600

    add logic to determin locale in ger_locale function

---
+++ 0x01-i18n/4-app.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+""" Basic flask app
+"""
+from flask import Flask, render_template, request
+from flask_babel import Babel, _
+
+app = Flask(__name__)
+babel = Babel(app)
+_.__doc__ = "gettext documentation"
+
+
+class Config(object):
+    """ Configure class is passed in object which is a
+        default value in pythonthat isnherits from the base class
+        but possess no attributes and cant be given any
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+_('home_title')
+_('home_header')
+
+
+@babel.localeselector
+def get_locale():
+    """ get locale
+    """
+    locale = request.args.get('locale')
+    if locale and locale in app.config['LANGUAGES']:
+        return locale
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def index():
+    """ index page
+    """
+    return render_template('3-index.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000', debug=True)
