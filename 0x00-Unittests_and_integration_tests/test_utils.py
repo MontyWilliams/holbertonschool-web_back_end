a97fe99199fa9b242e2b8ac9a7f46f3d62f77385
Montywilliams <3650@holbertonschool.com>
2022-10-11 15:44:44 -0600

    ad test for errors in any path

--- 0x00-Unittests_and_integration_tests/test_utils.py
+++ 0x00-Unittests_and_integration_tests/test_utils.py
@@ -16,8 +16,21 @@ class TestAccessNestedMap(unittest.TestCase):
         ({"a": {"b": 2}}, ("a", "b"),  2)
     ])
     def test_access_nested_map(self, nested_map, path, expected_result):
-        """ Test for correct functioning """
+        """ Test for correct functioning 
+        """
         self.assertEqual(access_nested_map(nested_map, path), expected_result)
+
+
+    @parameterized.expand([
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b"))
+    ])
+    def test_access_nested_map_exception(self, nested_map, path):
+        """ 
+        """
+        with self.assertRaises(KeyError):
+            access_nested_map(nested_map, path)
+
 
 if __name__ == '__main__':
     unittest.main()
