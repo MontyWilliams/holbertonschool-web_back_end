27326f07168ec4e731d9ce822b2609dda3628307
Montywilliams <3650@holbertonschool.com>
2022-11-15 16:13:31 -0600

    add 1 functions that exccept values and displays themm

---
+++ 0x02-queuing_system_in_js/1-redis_op.js
@@ -0,0 +1,20 @@
+import { createClient } from 'redis';
+import {print} from 'redis';
+
+const client = createClient();
+
+client.on('error', (err) => console.log('Redis client not connected to the server:', err));
+client.on('connect', () => console.log('Redis client connected to the server'));
+
+function setNewSchool(schoolName, value) {
+    client.set(schoolName, value);
+    print("Confirmation!: value set!")
+}
+
+function displaySchoolValue(schoolName) {
+    console.log(schoolName)
+}
+
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
