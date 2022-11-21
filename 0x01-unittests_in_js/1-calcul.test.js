6c983f04b7b1c203880e302350e7442a704a469e
Montywilliams <3650@holbertonschool.com>
2022-11-21 09:54:43 -0600

    correct assertions by passing type value

--- 0x01-unittests_in_js/1-calcul.test.js
+++ 0x01-unittests_in_js/1-calcul.test.js
@@ -16,23 +16,20 @@ describe('calculateNumber type == SUM', () => {
   describe('calculateNumber type == SUBTRACT', () => {
     // eslint-disable-next-line jest/prefer-expect-assertions
     it('Subtract positive ints', function () {
-      assert.strictEqual(calculateNumber('SUM', 1, 3), 4);
-      assert.strictEqual(calculateNumber('SUM', 1, 3.7), 5);
-      assert.strictEqual(calculateNumber('SUM', 1.2, 3.7), 5);
-      assert.strictEqual(calculateNumber('SUM', 1.5, 3.7), 6);
-      assert.strictEqual(calculateNumber('SUM', 3.7, 1), 5);
-      assert.strictEqual(calculateNumber('SUM', 3.7, 1.2), 5);
+      assert.strictEqual(calculateNumber('SUBTRACT', 1, 3), -2);
+      assert.strictEqual(calculateNumber('SUBTRACT', 1, 3.7), -3);
+      assert.strictEqual(calculateNumber('SUBTRACT', 1.2, 3.7), -3);
+      assert.strictEqual(calculateNumber('SUBTRACT', 1.5, 3.7), -2);
+      assert.strictEqual(calculateNumber('SUBTRACT', 3.7, 1), 3);
+      assert.strictEqual(calculateNumber('SUBTRACT', 3.7, 1.2), 3);
     });
   });
 
   describe('calculateNumber type == DIVIDE', () => {
     // eslint-disable-next-line jest/prefer-expect-assertions
     it('Divide positive ints', function () {
-      assert.strictEqual(calculateNumber('SUM', 1, 3), 4);
-      assert.strictEqual(calculateNumber('SUM', 1, 3.7), 5);
-      assert.strictEqual(calculateNumber('SUM', 1.2, 3.7), 5);
-      assert.strictEqual(calculateNumber('SUM', 1.5, 3.7), 6);
-      assert.strictEqual(calculateNumber('SUM', 3.7, 1), 5);
-      assert.strictEqual(calculateNumber('SUM', 3.7, 1.2), 5);
+      assert.strictEqual(calculateNumber('DIVIDE', 1, 3), 0.3333333333333333);
+      assert.strictEqual(calculateNumber('DIVIDE', 1, 2), 0.5);
+     
     });
   });
