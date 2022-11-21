b80da6f7d9069b4d4fd59a3b94ed359c6c320ce1
Montywilliams <3650@holbertonschool.com>
2022-11-21 03:05:45 -0600

    test cases for simple funstion

---
+++ 0x01-unittests_in_js/0-calcul.js
@@ -0,0 +1,5 @@
+// Used Math n
+function calculateNumber(a, b) {
+    return Math.round(a) + Math.round(b);
+  }
+  module.exports = calculateNumber;
---
+++ 0x01-unittests_in_js/test/0-calcul.test.js
@@ -0,0 +1,33 @@
+const assert = require('assert');
+const calculateNumber = require('../0-calcul.js')
+
+describe('calculateNumber', () => {
+    // eslint-disable-next-line jest/prefer-expect-assertions
+    it('postive intergers', function () {
+      assert.strictEqual(calculateNumber(1, 3), 4);
+      assert.strictEqual(calculateNumber(2.6, 1.1 ), 4);
+    });
+    // eslint-disable-next-line jest/prefer-expect-assertions
+    it('negative intergers', () => {
+      assert.strictEqual(calculateNumber(-1, -3), -4);
+      assert.strictEqual(calculateNumber(-1, -3.7), -5);
+      assert.strictEqual(calculateNumber(-1.2, -3.7), -5);
+      assert.strictEqual(calculateNumber(-1.6, -3.7), -6);
+      assert.strictEqual(calculateNumber(-3.7, -1), -5);
+      assert.strictEqual(calculateNumber(-3.7, -1.2), -5);
+    });
+  
+    it('returns NaN if a or b is infinite', () => {
+      assert.strictEqual(calculateNumber(Infinity, 1.6), Infinity);
+      assert.strictEqual(calculateNumber(1.6, Infinity), Infinity);
+      assert.strictEqual(calculateNumber(Infinity, Infinity), Infinity);
+      assert.strictEqual(calculateNumber(-Infinity, 1.6), -Infinity);
+      assert.strictEqual(calculateNumber(1.6, -Infinity), -Infinity);
+      assert.strictEqual(calculateNumber(-Infinity, -Infinity), -Infinity);
+    });
+    it('returns NaN if a or b is not a number', () => {
+      assert.strictEqual(calculateNumber('A', 1.6), NaN);
+      assert.strictEqual(calculateNumber(1.6, 'B'), NaN);
+      assert.strictEqual(calculateNumber('A', 'B'), NaN);
+    });
+  });
