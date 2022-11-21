e000f7e64942b93c4185a78d02ae591a7c516870
Montywilliams <3650@holbertonschool.com>
2022-11-21 03:23:24 -0600

    correct import

--- 0x01-unittests_in_js/0-calcul.test.js
+++ 0x01-unittests_in_js/0-calcul.test.js
@@ -1,5 +1,5 @@
 const assert = require('assert');
-const calculateNumber = require('../0-calcul.js')
+const calculateNumber = require('./0-calcul.js')
 
 describe('calculateNumber', () => {
     // eslint-disable-next-line jest/prefer-expect-assertions
