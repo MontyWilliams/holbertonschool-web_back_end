83d3168294ce90640b5cc124c7fe52fdf3f7be4c
Montywilliams <3650@holbertonschool.com>
2022-11-21 09:09:01 -0600

    add test for types being passed in

--- 0x01-unittests_in_js/1-calcul.test.js
+++ 0x01-unittests_in_js/1-calcul.test.js
@@ -1,9 +1,9 @@
 const assert = require('assert');
 const calculateNumber = require('./1-calcul.js')
 
-describe('calculateNumber', () => {
+describe('calculateNumber type == SUM', () => {
     // eslint-disable-next-line jest/prefer-expect-assertions
-    it('postive intergers', function () {
+    it('Calculate sum of positive ints', function () {
       assert.strictEqual(calculateNumber('SUM', 1, 3), 4);
       assert.strictEqual(calculateNumber('SUM', 1, 3.7), 5);
       assert.strictEqual(calculateNumber('SUM', 1.2, 3.7), 5);
@@ -11,5 +11,28 @@ describe('calculateNumber', () => {
       assert.strictEqual(calculateNumber('SUM', 3.7, 1), 5);
       assert.strictEqual(calculateNumber('SUM', 3.7, 1.2), 5);
     });
-   
+  });
+
+  describe('calculateNumber type == SUBTRACT', () => {
+    // eslint-disable-next-line jest/prefer-expect-assertions
+    it('Subtract positive ints', function () {
+      assert.strictEqual(calculateNumber('SUM', 1, 3), 4);
+      assert.strictEqual(calculateNumber('SUM', 1, 3.7), 5);
+      assert.strictEqual(calculateNumber('SUM', 1.2, 3.7), 5);
+      assert.strictEqual(calculateNumber('SUM', 1.5, 3.7), 6);
+      assert.strictEqual(calculateNumber('SUM', 3.7, 1), 5);
+      assert.strictEqual(calculateNumber('SUM', 3.7, 1.2), 5);
+    });
+  });
+
+  describe('calculateNumber type == DIVIDE', () => {
+    // eslint-disable-next-line jest/prefer-expect-assertions
+    it('Divide positive ints', function () {
+      assert.strictEqual(calculateNumber('SUM', 1, 3), 4);
+      assert.strictEqual(calculateNumber('SUM', 1, 3.7), 5);
+      assert.strictEqual(calculateNumber('SUM', 1.2, 3.7), 5);
+      assert.strictEqual(calculateNumber('SUM', 1.5, 3.7), 6);
+      assert.strictEqual(calculateNumber('SUM', 3.7, 1), 5);
+      assert.strictEqual(calculateNumber('SUM', 3.7, 1.2), 5);
+    });
   });
