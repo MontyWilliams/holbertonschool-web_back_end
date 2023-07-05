b562a069dae948aa5a7b5cb823eda0f3812d9838
Montywilliams <3650@holbertonschool.com>
2023-07-05 11:15:23 -0600

    rewrite

--- unittests_in_js/1-calcul.js
+++ unittests_in_js/1-calcul.js
@@ -1,19 +1,18 @@
 function calculateNumber(type, a, b) {
-  switch (type) {
-    case 'SUM':
-      return Math.round(a) + Math.round(b);
-      break;
-    case 'SUBTRACT':
-      return Math.round(a) - Math.round(b);
-      break;
-    case 'DIVIDE':
-      if (b === 0) {
-        return new Error('Error');
-        break;
-      } else {
-        return Math.round(a) / Math.round(b);
-      }
-      break;
+  const numA = Math.round(a);
+  const numB = Math.round(b);
+  let answer = 0;
+  if (type === 'SUM') {
+    answer = numA + numB;
+  } else if (type === 'SUBTRACT') {
+    answer = numA - numB;
+  } else if (type === 'DIVIDE') {
+    if (numB === 0) {
+      return 'Error';
+    }
+    answer = numA / numB;
   }
+  return answer;
 }
-module.exports = calculateNumber
+
+module.exports = calculateNumber;
