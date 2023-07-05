bb000a5868708b04d6543239e9dc4a0c8108d99f
Montywilliams <3650@holbertonschool.com>
2023-07-05 10:48:19 -0600

    stop rounding for division

--- unittests_in_js/1-calcul.js
+++ unittests_in_js/1-calcul.js
@@ -8,9 +8,9 @@ function calculateNumber(type, a, b) {
       break;
     case 'DIVIDE':
       if (b === 0) {
-        return 'Error';
+        return 'Error'; 
       } else {
-        return Math.round(a) / Math.round(b);
+        return a / b;
       }
       break;
   }
