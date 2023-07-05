b5d8c99e8673a3df6e332c9b23d575db3809bcb1
Montywilliams <3650@holbertonschool.com>
2023-07-05 08:25:16 -0600

    add tests for rounded numbers

--- unittests_in_js/0-calcul.test.js
+++ unittests_in_js/0-calcul.test.js
@@ -3,7 +3,8 @@ const calculateNumber = require('./0-calcul')
 
 describe('calculateNumber', () => {
   it('should add positive integers', () => {
-    assert.strictEqual(calculateNumber(2, 2), 4);
-    assert.strictEqual(calculateNumber(2, 7), 9)
+    assert.strictEqual(calculateNumber(1.8, 2), 4);
+    assert.strictEqual(calculateNumber(2, 6.9), 9)
+    assert.strictEqual(calculateNumber(1/1, 7.1), 8)
   })
 })
