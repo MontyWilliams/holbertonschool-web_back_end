d5a6dc477412180a362caf7f1f4eec7eb720d67d
Montywilliams <3650@holbertonschool.com>
2023-07-11 08:32:33 -0600

    task 5

---
+++ unittests_in_js/5-payment.test.js
@@ -0,0 +1,22 @@
+const sinon = require('sinon');
+const { expect } = require('chai');
+const sendPaymentRequestToApi = require('./5-payment');
+
+describe('sendPaymentRequestToApi', () => {
+  let spy;
+  beforeEach(() => {
+    spy = sinon.spy(console, 'log');
+  });
+
+  afterEach(() => {
+    spy.restore();
+  });
+  it('sendPaymentRequestToApi 100 20', () => {
+    sendPaymentRequestToApi(100, 20);
+    expect(spy.calledWith('The total is: 120')).to.be.true;
+  });
+  it('sendPaymentRequestToApi 10 10', () => {
+    sendPaymentRequestToApi(10, 10);
+    expect(spy.calledWith('The total is: 20')).to.be.true;
+  });
+});
